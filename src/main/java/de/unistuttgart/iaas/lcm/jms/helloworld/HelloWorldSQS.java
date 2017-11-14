f49bdc91f6b99b26564caaeb7b28c3aa10a6f259
Florian Haupt <F7502@users.noreply.github.com>
2017-11-14 18:45:57 +0100

    apply consistent indentation and line wraps (eclipse auto format, tabs only, line break after 120 characters)

--- src/main/java/de/unistuttgart/iaas/lcm/jms/helloworld/HelloWorldSQS.java
+++ src/main/java/de/unistuttgart/iaas/lcm/jms/helloworld/HelloWorldSQS.java
@@ -30,97 +30,102 @@ import com.amazonaws.regions.Regions;
  */
 public class HelloWorldSQS {
 
-    final static Logger logger = LoggerFactory.getLogger(HelloWorldSQS.class);
+	final static Logger logger = LoggerFactory.getLogger(HelloWorldSQS.class);
 
-    private static QueueConnectionFactory conFactory;
-    private static QueueConnection connection;
-    private static QueueSession session;
-    private static Queue testQueue;
-    private static QueueSender sender;
-    private static QueueReceiver receiver;
+	private static QueueConnectionFactory conFactory;
+	private static QueueConnection connection;
+	private static QueueSession session;
+	private static Queue testQueue;
+	private static QueueSender sender;
+	private static QueueReceiver receiver;
 
-    // name of SQS queue, has to be created in advance 
-    private static final String queueName = "jms-test-queue";
-    // properties file containing the AWS API credentials (access key and secret key)
-    private static final String awsCredentialsFile = "src\\main\\resources\\aws.properties";
-    
-    public static void main(String[] args) {
-	try {
-	    // initiate everything we need
-	    init();
-	    // send a text message to queue
-	    send();
-	    // receive a text message from queue
-	    receive();
-	} finally {
-	    try {
-		connection.close();
-	    } catch (JMSException e) {
-		e.printStackTrace();
-	    }
-	}
-    }
+	// name of SQS queue, has to be created in advance
+	private static final String queueName = "jms-test-queue";
+	// properties file containing the AWS API credentials (access key and secret
+	// key)
+	private static final String awsCredentialsFile = "src\\main\\resources\\aws.properties";
 
-    private static void init() {
-	// http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-java-message-service-jms-client.html
-	try {
-	    // Create the connection factory using the properties file
-	    // credential provider.
-	    conFactory = SQSConnectionFactory.builder().withRegion(Region.getRegion(Regions.EU_WEST_1))
-		    .withAWSCredentialsProvider(new PropertiesFileCredentialsProvider(awsCredentialsFile)).build();
-	    
-	    // create connection.
-	    connection = conFactory.createQueueConnection();
-	    // create session
-	    // https://docs.oracle.com/javaee/7/api/javax/jms/Session.html
-	    session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-	    // lookup queue (has to be created before, using e.g. the AWS management console)
-	    testQueue = session.createQueue(queueName);
-	    // create sender and receiver
-	    sender = session.createSender(testQueue);
-	    receiver = session.createReceiver(testQueue);
-	    // start connection (!)
-	    connection.start();
-
-	} catch (JMSException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-    }
-
-    private static void send() {
-	try {
-	    // create and send text message
-	    TextMessage msg = session.createTextMessage("Hello World!");
-	    // set property
-	    msg.setStringProperty("sender", "hauptfn");
-	    sender.send(msg);
-	    logger.debug("Sent message: {}", msg);
-	    logger.info("Sent: {}", msg.getText());
-	} catch (JMSException e) {
-	    e.printStackTrace();
-	}
-    }
-
-    private static void receive() {
-	try {
-	    // receive message
-	    Message msg = receiver.receive();
-	    logger.debug("Received message: {}", msg);
-	    if (msg instanceof TextMessage) {
-		logger.info("Received: {}", ((TextMessage) msg).getText());
-		@SuppressWarnings("unchecked")
-		Enumeration<String> propNames = msg.getPropertyNames(); 
-		while (propNames.hasMoreElements()) {
-		    String pn = propNames.nextElement();
-		    logger.info("- {} = {}", pn, msg.getObjectProperty(pn));
+	public static void main(String[] args) {
+		try {
+			// initiate everything we need
+			init();
+			// send a text message to queue
+			send();
+			// receive a text message from queue
+			receive();
+		} finally {
+			try {
+				connection.close();
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
 		}
-	    }
-	    // required when session is set to Session.CLIENT_ACKNOWLEDGE
-	    //msg.acknowledge();
-	} catch (JMSException e) {
-	    e.printStackTrace();
 	}
-    }
+
+	private static void init() {
+		// http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-java-message-service-jms-client.html
+		try {
+			// Create the connection factory using the properties file
+			// credential provider.
+			conFactory = SQSConnectionFactory
+							.builder()
+							.withRegion(Region.getRegion(Regions.EU_WEST_1))
+							.withAWSCredentialsProvider(new PropertiesFileCredentialsProvider(awsCredentialsFile))
+							.build();
+
+			// create connection.
+			connection = conFactory.createQueueConnection();
+			// create session
+			// https://docs.oracle.com/javaee/7/api/javax/jms/Session.html
+			session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			// lookup queue (has to be created before, using e.g. the AWS management
+			// console)
+			testQueue = session.createQueue(queueName);
+			// create sender and receiver
+			sender = session.createSender(testQueue);
+			receiver = session.createReceiver(testQueue);
+			// start connection (!)
+			connection.start();
+
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void send() {
+		try {
+			// create and send text message
+			TextMessage msg = session.createTextMessage("Hello World!");
+			// set property
+			msg.setStringProperty("sender", "hauptfn");
+			sender.send(msg);
+			logger.debug("Sent message: {}", msg);
+			logger.info("Sent: {}", msg.getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void receive() {
+		try {
+			// receive message
+			Message msg = receiver.receive();
+			logger.debug("Received message: {}", msg);
+			if (msg instanceof TextMessage) {
+				logger.info("Received: {}", ((TextMessage) msg).getText());
+				@SuppressWarnings("unchecked")
+				Enumeration<String> propNames = msg.getPropertyNames();
+				while (propNames.hasMoreElements()) {
+					String pn = propNames.nextElement();
+					logger.info("- {} = {}", pn, msg.getObjectProperty(pn));
+				}
+			}
+			// required when session is set to Session.CLIENT_ACKNOWLEDGE
+			// msg.acknowledge();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
